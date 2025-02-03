6b49b85d8a0510a3edf36238855b811db9b012c6
Al- Mustarik <almustarik@gmail.com>
2025-02-03 22:54:59 +0600

    feat: confirmCheckbox id

--- local-order.js
+++ local-order.js
@@ -568,7 +568,7 @@ document.addEventListener('DOMContentLoaded', async function () {
   updateStepper(currentStep);
 
   // Enable/Disable "Next" button based on checkbox state
-  // const confirmCheckbox = document.getElementById('confirm-checkbox');
+  const confirmCheckbox = document.getElementById('confirm-checkbox');
   // const nextStepButton = document.getElementById('next-step-button');
 
   confirmCheckbox.addEventListener('change', function () {
